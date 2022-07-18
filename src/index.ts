90d08e47f9ac65beb1361dd9457e8ad62453edb1
Ryan Page <ryanapage@gmail.com>
2022-07-17 19:41:56 -0700

    bit of a refactor

--- src/index.ts
+++ src/index.ts
@@ -1,74 +1,19 @@
-import fs from 'fs';
 import path from 'path';
-import bluebird from 'bluebird';
 import { parseTCGPlayerCsv } from './util/file';
 import logger from './util/logger';
 import * as skryfall from './util/skryfall';
-import { cleanName } from './util/tcgplayer';
-import SetReplace from '../set-replace.json';
-
-import { TCGPlayerRow } from '../type/csv';
-import axios from 'axios';
 
 async function main() {
-    logger.info('downloading updated skryfall database');
+    // await skryfall.saveBulkData();
+    logger.info('Downloaded updated skryfall database.');
 
-    await skryfall.saveBulkData();
-
-    const skryfallData = require('../default-cards.json');
-
-    logger.info('Beginning import of CSV file.');
     const output = await parseTCGPlayerCsv(path.join(__dirname, '../test/TCGplayerCardList.csv'));
+    logger.info('Retrieved TCGPlayer csv from file.');
 
-    for (const card of output) {
-        card.Name = cleanName(card.Name as string); 
+    const skryfallData = skryfall.convertToSkryfall(output);
+    logger.info('Converted data to skryfall format.');
 
-        // @ts-ignore
-        if (SetReplace[card['Set Code'] as string])
-                 // @ts-ignore
-                 card['Set Code'] = SetReplace[card['Set Code'] as string];
-
-        const filtered = skryfallData.filter((s: any) => {
-            // logger.info(s.name + ' ' + card.Name);
-            // logger.info(s.set + ' ' + card['Set Code']?.toLowerCase());
-            return s.name === card.Name && s.set === card['Set Code']?.toLowerCase();
-        });
-        
-        logger.info(filtered.length);
-    }
-
-
-
-
-
-
-
-
-    // const executions = [];
-
-    // // logger.info(JSON.stringify(output, null, 4));
-    // for (const card of output) {
-    //     // if (card.Name !== 'Two-Handed Axe')
-    //     //     continue;
-
-    //     card.Name = cleanName(card.Name as string);
-
-    //     // @ts-ignore
-    //     if (SetReplace[card['Set Code'] as string])
-    //         // @ts-ignore
-    //         card['Set Code'] = SetReplace[card['Set Code'] as string];
-
-    //     // @ts-ignore
-    //     // executions.push(skryfall.searchCardByName(card.Name, card['Set Code']));
-    //     executions.push(card);
-    // }
-
-    // const res = await bluebird.map(executions, async (card: TCGPlayerRow) => {
-    //     // @ts-ignore
-    //     await skryfall.searchCardByName(card.Name, card['Set Code']); 
-    // },{
-    //     concurrency: 50
-    // });
+    
     
 }
 
--- src/util/skryfall.ts
+++ src/util/skryfall.ts
@@ -1,7 +1,10 @@
 import Axios from 'axios'
 import fs from 'fs';
 import path from 'path';
+import { TCGPlayerRow } from '../../type/csv';
 import logger from './logger';
+import { cleanName } from './tcgplayer';
+import SetReplace from '../../set-replace.json';
 
 const axios = Axios.create({
     baseURL: 'https://api.scryfall.com'
@@ -34,4 +37,54 @@ export const saveBulkData = async () => {
     fs.writeFileSync(
         path.join(__dirname, '../../default-cards.json'), 
         JSON.stringify(defaultCards.data, null, 4));
+}
+
+/**
+ * Convert all cards to skryfall format
+ */
+export const convertToSkryfall = async (cards: TCGPlayerRow[]) => {
+    const skryfallData = require('../../default-cards.json');
+
+    const toReturn: any = [];
+
+    for (const card of cards) {
+        card.Name = cleanName(card.Name as string); 
+
+        card['Set Code'] = card['Set Code']?.toLowerCase();
+
+        if (card['Card Number']?.endsWith('p'))
+            card['Card Number'] = card['Card Number'].substring(0, card['Card Number'].length - 1);
+
+        // promo-pack edge case
+        if (card['Set Code']?.startsWith('pp') && card['Set Code'].length > 3) {
+            card['Set Code'] = card['Set Code'].slice(2);
+        }
+
+        // @ts-ignore
+        if (SetReplace[card['Set Code']?.toLowerCase()]) {
+            // @ts-ignore
+            card['Set Code'] = SetReplace[card['Set Code']];
+        }
+
+        const filtered = skryfallData.filter((s: any) => {
+
+            const checkForFoil = s.foil === (card.Printing === 'Foil')
+                || s.nonfoil === (card.Printing === 'Normal');
+
+            return s.collector_number === card['Card Number'] 
+                    && s.set === card['Set Code']?.toLowerCase()
+                    && checkForFoil;
+        });
+
+        const filteredFromProductId = skryfallData.filter((s: any) => {
+            return s.tcgplayer_id === Number.parseInt(card['Product ID'] as string);
+        });
+        
+        if (filtered.length !== 1 && filteredFromProductId !== 1)
+            throw new Error(`Could not find skryfall information for card: ` + JSON.stringify(card, null, 4));
+        
+        toReturn.push(filteredFromProductId[0] || filtered[0]);
+    }
+
+    return toReturn;
 }
